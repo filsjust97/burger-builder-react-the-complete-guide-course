741e4f38167b829ecf92286eb0cf61dbc805a3f1
Filipe Martins Lourenco <filipe.martins.lourenco@blip.pt>
2020-05-11 11:49:05 +0100

    Add Routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,28 +87,19 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'Test Street 1',
-                    zipCode: '441333',
-                    country: 'Germany'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMehtod: 'fastest'
-        }
+            const queryParams = [];
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
+            for (let i in this.state.ingredients) {
+                queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+            }
+
+            queryParams.push('price=' + this.state.totalPrice);
+            
+            const queryString = queryParams.join('&');
+
+            this.props.history.push({
+                pathname: '/checkout',
+                search: `?${queryString}`
             });
     }
 
